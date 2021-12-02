e2573a3aabe2427046384360caec6bda2f34b5fb
purab <purabdk@gmail.com>
2021-12-02 18:41:45 +0530

    added logging xml configuration file

--- src/main/java/in/purabtech/ep/EpApplication.java
+++ src/main/java/in/purabtech/ep/EpApplication.java
@@ -1,12 +1,15 @@
 package in.purabtech.ep;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EpApplication {
 
 	public static void main(String[] args) {
+		log.info("starting app...");
 		SpringApplication.run(EpApplication.class, args);
 	}
 
